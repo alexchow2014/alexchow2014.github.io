8c80611fc056aa40744294a3f4e15a030eef2405
alexchow2014 <alexchow2014@gmail.com>
2023-01-17 23:34:52 +0800

    2023/01/17 - initial upload

---
+++ personal/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+// Pages
+import WebPage from './pages/page';
+
+function App() {
+  return (
+    <WebPage />
+  );
+
+}
+
+export default App;
---
+++ personal/src/pages/page.js
@@ -0,0 +1,13 @@
+import './page.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+
+function WebPage() {
+    return (
+        <div className="App">
+            <div>SSSS</div>
+        </div>
+    );
+  }
+  
+  export default WebPage;
