3f54be9853459a0b6d858a4a22dcce87e7fa1e87
alexchow2014 <alexchow2014@gmail.com>
2023-02-14 21:20:52 +0800

    2023/02/14 - update
    Update the footer

--- personal/src/components/footer.js
+++ personal/src/components/footer.js
@@ -5,7 +5,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="container col-12">
-            <div>© Copyright 2023 Chow Tsz Kui, Alex. Last updated: January 28, 2023.</div>
+            <div>© Copyright 2023 Chow Tsz Kui, Alex. Last updated: 14 February, 2023.</div>
         </div>
     );
 }
