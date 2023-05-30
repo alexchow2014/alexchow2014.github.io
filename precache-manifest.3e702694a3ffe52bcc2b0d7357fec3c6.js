5bb75a27ecf921e3bac558ddd2fdd5b2685c8c39
alexchow2014 <alexchow2014@gmail.com>
2023-05-30 23:29:32 +0800

    Updates

---
+++ precache-manifest.3e702694a3ffe52bcc2b0d7357fec3c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4331e15c5569569fd1",
+    "url": "/static/css/main.6e7e0280.chunk.css"
+  },
+  {
+    "revision": "6a4331e15c5569569fd1",
+    "url": "/static/js/main.9bd4f7a2.chunk.js"
+  },
+  {
+    "revision": "a8a43faf50e8d4bf83b2",
+    "url": "/static/js/runtime~main.bb45a6d3.js"
+  },
+  {
+    "revision": "7f1705a8335b9adb302d",
+    "url": "/static/css/2.aad331a9.chunk.css"
+  },
+  {
+    "revision": "7f1705a8335b9adb302d",
+    "url": "/static/js/2.7807cb45.chunk.js"
+  },
+  {
+    "revision": "1f0fc60ab0504fabf343",
+    "url": "/static/js/3.180fc2d3.chunk.js"
+  },
+  {
+    "revision": "b5bc06cd6b607ce28f9b612d908cc043",
+    "url": "/static/media/profile.b5bc06cd.png"
+  },
+  {
+    "revision": "beac721fa06ad6ef84b3432ea6fd5979",
+    "url": "/static/media/ty_img.beac721f.png"
+  },
+  {
+    "revision": "24e325588c92492fc7c2f06a76568882",
+    "url": "/static/media/ts_box3_9.24e32558.jpg"
+  },
+  {
+    "revision": "9d94fa9813a98f422378b125ebda11ad",
+    "url": "/index.html"
+  }
+];
